236fef95998dfbd4fe0038056a8b82f90d92e899
Fredsoma <somayukirihafred@gmail.com>
2025-03-10 20:38:11 +0100

    first commit

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -13,7 +13,7 @@ const LoginPage = () => {
 
   const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const response = await fetch("http://localhost:8000/auth/login", {
+    const response = await fetch("https://inventory-management-app-v2.onrender.com/auth/login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
